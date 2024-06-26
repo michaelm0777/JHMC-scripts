2176b2a3a4de4cbb122f3730fa2b5ddb6a3c4902
plasmsapoke <dwang2@imsa.edu>
2024-06-26 12:21:35 -0500

    Add files via upload

---
+++ secrets.js
@@ -0,0 +1,6 @@
+module.exports = {
+    apiKey: "patTP2wHQI4u7H1tb.c18dd3cdace3c947afce86604cebe79c3e85a53a764514ff87ebf8a8966650db", //"keySppkhSfTKtYKMV", // API Key for Airtable
+    baseID: "appN77Z7XRfBIGQ5y", //"appwHybdVDpD95kLa", // Base ID for Airtable
+    sampleTestId: "recYkk156Rx9bcKYr", //"recHULdb4DHbCBeE3", // ID for the sample test, given special permissions
+    masterTestId: "recuMfrScqTqQBqGj", //"recdmGDYac3sh0UmV" // ID for the master test, also given special permissions but not public
+}
